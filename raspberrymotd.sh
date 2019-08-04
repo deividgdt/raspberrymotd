a5a5f728f55d8146f362d7558aa592ac0973759a
David <42324150+deividgdt@users.noreply.github.com>
2019-08-04 02:12:58 +0200

    0.3

--- raspberrymotd.sh
+++ raspberrymotd.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 ################  MOTD FOR RASPBERRY  ####################
 ###############  github.com/deividgdt  ###################
 ##########################################################
 
-RED='\e[31m'   
-GREEN='\e[32m' 
+RED='\e[31m'
+GREEN='\e[32m'
 NOCOL='\e[0m'
 UP=`uptime --pretty`
 CPUSAGE=`cat /proc/loadavg | awk '{print $1" "$2" "$3}'`
 TOTALMEM=`awk 'NR==1 {printf "%0.0f MB",$2/1024}' /proc/meminfo`
 USEDMEM=`awk 'NR==2 {printf "%0.0f/",$2/1024}' /proc/meminfo`
-DISKUSAGE=`df --human-readable | grep --extended-regexp "/$" | awk '{print $4"/"$2" "$5}'`
+DISKUSAGE=`df --human-readable | grep --extended-regexp "/$" | awk '{print $4"/"$2" Used: "$5}'`
 NETDEV=`ip route get 8.8.8.8 | head --lines=1 | awk '{print $5}'`
 IPADDR=`ip -oneline -family inet address show | grep ${NETDEV} | awk '{print $4}'| cut --fields=1 --delimiter="/"`
 TEMP=`/opt/vc/bin/vcgencmd measure_temp | cut --fields=2 --delimiter="="`
@@ -34,10 +35,11 @@ echo -e "       :s-    oyo:...:oy.    /o"
 echo -e "        -oo/::yyysooooyy+::+o:"
 echo -e "           :+yy+      .syo/."
 echo -e "              ./o+//+o+-"
-echo -e "                  ...\n${GREEN}"
+echo -e "                  ...\n${NOCOL}"
+echo -e "     System-------> `uname -snrvm`"
 echo -e "     Up Time------> ${UP}"
 echo -e "     CPU Usage----> ${CPUSAGE}"
 echo -e "     RAM Usage----> ${USEDMEM}${TOTALMEM}"
 echo -e "     SD Usage-----> ${DISKUSAGE}"
 echo -e "     IP Address---> ${IPADDR}"
-echo -e "     Temperature--> ${TEMP}${NOCOL}"
+echo -e "     Temperature--> ${TEMP}"
