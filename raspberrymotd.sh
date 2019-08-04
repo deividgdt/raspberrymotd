ec03bc80cfb9d3b4fcf20179cd5879ca9a1da21a
David <42324150+deividgdt@users.noreply.github.com>
2019-08-04 19:07:47 +0200

    0.4

--- raspberrymotd.sh
+++ raspberrymotd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 ################  MOTD FOR RASPBERRY  ####################
 ###############  github.com/deividgdt  ###################
-##########################################################
+##########################0.4#############################
 
 RED='\e[31m'
 GREEN='\e[32m'
 NOCOL='\e[0m'
 UP=`uptime --pretty`
-CPUSAGE=`cat /proc/loadavg | awk '{print $1" "$2" "$3}'`
+CPUSAGE=`cat /proc/loadavg | awk '{print "1 minute: "$1"/ 5 minutes: "$2"/ 15 minutes: "$3}'`
 TOTALMEM=`awk 'NR==1 {printf "%0.0f MB",$2/1024}' /proc/meminfo`
-USEDMEM=`awk 'NR==2 {printf "%0.0f/",$2/1024}' /proc/meminfo`
+USEDMEM=`awk 'NR==2 {printf "%0.0f MB",$2/1024}' /proc/meminfo`
 DISKUSAGE=`df --human-readable | grep --extended-regexp "/$" | awk '{print $4"/"$2" Used: "$5}'`
 NETDEV=`ip route get 8.8.8.8 | head --lines=1 | awk '{print $5}'`
 IPADDR=`ip -oneline -family inet address show | grep ${NETDEV} | awk '{print $4}'| cut --fields=1 --delimiter="/"`
@@ -20,9 +20,9 @@ echo -e "         ${GREEN}.-::--.       .-:::-."
 echo -e "      /+o//:::/+o+. -+o+/::://oo:"
 echo -e "      o+  .-.    -s/o     -.  .y/"
 echo -e "      :y-   .:::. oy- -/:-    :s."
-echo -e "       :s:     .osyyys:     ./s-"
-echo -e "        ./s+:-:+yyysyys/:-/+s:${NOCOL}"
-echo -e "        ${RED}.++/--os:    ./s::/os:"
+echo -e "       :s:     .osyyys:     ./s-${NOCOL}"
+echo -e "        ${RED}./s+:-:+yyysyys/:-/+s"
+echo -e "        .++/--os:    ./s::/os::"
 echo -e "       .s:  :sys-    .:ys/  -y+"
 echo -e "       :s:+yo/--:oyyy/-.-:o+-oy"
 echo -e "     .+o+yy-      sy:      /y++s/"
@@ -39,7 +39,7 @@ echo -e "                  ...\n${NOCOL}"
 echo -e "     System-------> `uname -snrvm`"
 echo -e "     Up Time------> ${UP}"
 echo -e "     CPU Usage----> ${CPUSAGE}"
-echo -e "     RAM Usage----> ${USEDMEM}${TOTALMEM}"
+echo -e "     RAM Usage----> Free RAM: ${USEDMEM} of ${TOTALMEM}"
 echo -e "     SD Usage-----> ${DISKUSAGE}"
 echo -e "     IP Address---> ${IPADDR}"
 echo -e "     Temperature--> ${TEMP}"
