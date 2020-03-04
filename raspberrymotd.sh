2bc8e03105d49d302260b96c4d013276dd0730a4
David <42324150+deividgdt@users.noreply.github.com>
2020-03-04 21:10:36 +0100

    Small changes
    -Now the Free mem value is correct
      USEDMEM has been deleted
      AVAIMEM has  been created

--- raspberrymotd.sh
+++ raspberrymotd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ################  MOTD FOR RASPBERRY  ####################
 ###############  github.com/deividgdt  ###################
-##########################0.4#############################
+##########################################################
 
 RED='\e[31m'
 GREEN='\e[32m'
@@ -9,7 +9,7 @@ NOCOL='\e[0m'
 UP=`uptime --pretty`
 CPUSAGE=`cat /proc/loadavg | awk '{print "1 minute: "$1"/ 5 minutes: "$2"/ 15 minutes: "$3}'`
 TOTALMEM=`awk 'NR==1 {printf "%0.0f MB",$2/1024}' /proc/meminfo`
-USEDMEM=`awk 'NR==2 {printf "%0.0f MB",$2/1024}' /proc/meminfo`
+AVAIMEM=`awk 'NR==3 {printf "%0.0f MB",$2/1024}' /proc/meminfo`
 DISKUSAGE=`df --human-readable | grep --extended-regexp "/$" | awk '{print $4"/"$2" Used: "$5}'`
 NETDEV=`ip route get 8.8.8.8 | head --lines=1 | awk '{print $5}'`
 IPADDR=`ip -oneline -family inet address show | grep ${NETDEV} | awk '{print $4}'| cut --fields=1 --delimiter="/"`
@@ -39,7 +39,7 @@ echo -e "                  ...\n${NOCOL}"
 echo -e "     System-------> `uname -snrvm`"
 echo -e "     Up Time------> ${UP}"
 echo -e "     CPU Usage----> ${CPUSAGE}"
-echo -e "     RAM Usage----> Free RAM: ${USEDMEM} of ${TOTALMEM}"
+echo -e "     RAM Usage----> Free RAM: ${AVAIMEM} of ${TOTALMEM}"
 echo -e "     SD Usage-----> ${DISKUSAGE}"
 echo -e "     IP Address---> ${IPADDR}"
 echo -e "     Temperature--> ${TEMP}"
